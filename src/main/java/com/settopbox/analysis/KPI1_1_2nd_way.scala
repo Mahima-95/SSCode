1119537ca80b8796c27339be856fb1e1d1a46991
magrawal1 <magrawal1@xavient.com>
2018-01-19 12:19:15 +0530

    replace file

--- src/main/java/com/settopbox/analysis/KPI1_1_2nd_way.scala
+++ src/main/java/com/settopbox/analysis/KPI1_1_2nd_way.scala
@@ -1,6 +1,7 @@
 package com.settopbox.analysis
 
 import org.apache.spark.sql.SparkSession
+import scala.xml.XML
 
 object KPI1_1_2nd_way {
 
@@ -14,6 +15,21 @@ object KPI1_1_2nd_way {
       .filter(data => {
         data._1.==(100)
       })
+      .map(data => {
+        var xmlTags = data._2.split("\\^")
+        (xmlTags(4), data)
+      })
+      .map(data => {
+        var xml = XML.loadString(data._1)
+        var innerTags = xml \\ "nv"
+        var xml0 = XML.loadString(innerTags.theSeq(3).toString())
+        var duration = xml0.attribute("v").get.toString()
+        (duration, data._2)
+      }).groupByKey()
+      .map(data => {
+        (data._1.max, data._2)
+      }).sortBy(rec => rec._2, false).take(5)
+    result.foreach(println)
     spark.stop
   }
 }
